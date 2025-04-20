fb8184402780073a60f07788e5ad8f85f6cb1c31
Rushika Jayasinghe <Rushika@CreativeSoftware.com>
2025-04-20 19:58:17 +0530

    Initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,21 +5,21 @@ const About = () => {
         <p className="text-slate-300">
           I’m a passionate Data Engineer experienced in building efficient data pipelines, automating ETL processes, and working with Azure MDP. I love solving complex data problems using Python, SQL, and cloud technologies.
         </p>
-        {/* <a
-          href="/Rushika Jayasinghe Resume.pdf"
+        <a
+          href="/Rushika_Jayasinghe_Resume.pdf"
           download
           className="inline-block mt-4 px-6 py-2 bg-cyan-500 text-white rounded hover:bg-cyan-600"
         >
           Download Resume
-        </a> */}
-        <a
+        </a>
+        {/* <a
         href="/Rushika_Jayasinghe_Resume.pdf"
         target="_blank"
         rel="noopener noreferrer"
         className="text-blue-500 underline"
         >
         View Resume
-        </a>
+        </a> */}
       </section>
     );
   };
